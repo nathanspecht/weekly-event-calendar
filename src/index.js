3d65e5961777419e0e56934d92941b92805941e4
Nathan Specht <nathanspecht@gmail.com>
2017-08-03 14:25:32 -0700

    Create lib

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Schedule from './components/Schedule'
-import registerServiceWorker from './registerServiceWorker'
-import './index.css'
-import 'react-virtualized/styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Schedule from 'components/Schedule';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'react-virtualized/styles.css';
 
-ReactDOM.render(<Schedule />, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(<Schedule />, document.getElementById('root'));
+registerServiceWorker();
